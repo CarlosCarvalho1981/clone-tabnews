decd995a558de629b13db4009184be66b9b0e05e
Carlos Eduardo Carvalho <carlos.e.carvalho@gmail.com>
2026-03-21 22:15:08 +0000

    finish ´api/v1/status´ endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS SUM;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Tudo certo" });
+  const updatedAt = new Date().toISOString();
+  const versionResult = await database.query("SHOW server_version;");
+  const maxConnResult = await database.query("SHOW max_connections");
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionResult.rows[0].server_version,
+        max_connections: parseInt(maxConnResult.rows[0].max_connections),
+        used_connections: parseInt(usedConnResult.rows[0].count),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,4 +1,18 @@
 test("Get to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.database.version).toEqual("16.13");
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
+  expect(
+    responseBody.dependencies.database.used_connections,
+  ).toBeGreaterThanOrEqual(1);
+  expect(
+    responseBody.dependencies.database.used_connections,
+  ).toBeLessThanOrEqual(responseBody.dependencies.database.max_connections);
 });
